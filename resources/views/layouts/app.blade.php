54d7175d98fc0fce6fe5aa834066c08ee18586b0
William <willnrf@gmail.com>
2017-11-29 11:17:08 -0200

    Modificação página logada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ url('/') }}">Cooperativas</a></li>
+                            <li><a href="{{ url('/') }}">Pesquisadores</a></li>
+                            <li><a href="{{ url('/') }}">Municípios</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
